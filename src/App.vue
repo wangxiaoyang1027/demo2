c989643a1b5b9d87bc08d0c8f04a1fc189b9dd1e
wangxiaoyang1027 <wangxy_ly@aliyun.comm>
2019-12-18 23:09:31 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div >
+    <TabBar></TabBar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabBar from './components/tabbar/tabbar.vue'
+
+// import './assets/css/base.css'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TabBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0;
+  padding: 0;
 }
+
+
+
 </style>
---
+++ src/components/tabbar/tabbar.vue
@@ -0,0 +1,34 @@
+<template>
+<div id='tabbar'>
+    <div class='tab_bar_item'>我的</div>
+    <div class='tab_bar_item'>购物车</div>
+    <div class="tab_bar_item">item3</div>
+    <div class="tab_bar_item">item4</div>
+</div>
+</template>
+
+<script>
+export default {
+    name:'tab_bar'
+}
+</script>
+
+<style scoped>
+
+#tabbar{
+    position: fixed;
+    display: flex;
+    background-color: #f6f6f6;
+    left:0px;
+    right:0px;
+    bottom:0px;
+}
+
+
+.tab_bar_item{
+    flex:1;
+    text-align: center;
+    height: 49px;
+}
+
+</style>
